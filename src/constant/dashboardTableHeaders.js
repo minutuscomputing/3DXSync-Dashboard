3b6dbe53e727d22d706b5aaa7df988b48dbc4db7
pranoti-minutus <pranoti.patil@minutuscomputing.com>
2025-06-27 16:01:54 +0530

    feat: added centralized headers.js file for reusable table headers

---
+++ src/constant/dashboardTableHeaders.js
@@ -0,0 +1,6 @@
+export const tableHeaders = [
+  { title: 'CO Number', key: 'coNumber', sortable: true, align: 'start' },
+  { title: 'Status', key: 'status', sortable: true, align: 'start' },
+  { title: 'Date', key: 'date', sortable: true, align: 'start' },
+  { title: 'Description', key: 'description', align: 'start' },
+];
